801fec38a3346d7b941c0467de35773441ea4efe
Emily <93992216+em-i-ly@users.noreply.github.com>
2023-12-23 23:40:32 +0100

    finished task 3

--- Task 3/app.js
+++ Task 3/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const cors = require('cors'); 
+const cors = require('cors');
 const app = express();
 const http = require('http');
 const path = require('path');
 const httpServer = http.Server(app);
-const { Server } = require("socket.io");
+const {Server} = require("socket.io");
+const {query} = require("express");
 const io = new Server(httpServer);
 
 const QueryEngine = require('@comunica/query-sparql').QueryEngine;
@@ -20,45 +21,82 @@ app.use(express.json())
 app.use(express.static('public'))
 
 
-
 app.get('/', (req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.sendFile(path.join(__dirname, 'index.html'));
-  });
+});
 
-  app.get('/movies', (req, res) => {
-      let input_url = req.query.url
-      console.log("input url: ", input_url)
-      res.statusCode = 200
-      res.setHeader("Content-Type", "text/html");
-      res.sendFile(path.join(__dirname, 'index.html'));
-      handle_query(input_url)
-  });
+app.get('/movies', async (req, res) => {
+    let input_url = req.query.url
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(path.join(__dirname, 'index.html'));
+    await handle_query(input_url)
+});
 
-  async function handle_query(input_url){
-    movie_urls=[] //List of movies in the pod
-    engine.queryBindings(``, { //TODO: complete SPARQL query
-    sources: [input_url],
-  }).then(function (bindingsStream) {
-    bindingsStream.on('data', function (data) {
-      movie_urls.push(data.get('v').value)
+app.get('/search', async (req, res) => {
+    let url_keyword = req.query.keyword_url
+    let keyword = req.query.keyword
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(path.join(__dirname, 'index.html'));
+    await handle_query_by_movie_name(url_keyword, keyword)
+});
+
+
+async function handle_query(input_url) {
+    let movie_urls = [] //List of movies in the pod
+    await engine.queryBindings(`SELECT ?v WHERE { ?container <http://www.w3.org/ns/ldp#contains> ?v . }`, {
+        sources: [input_url],
+    }).then(function (bindingsStream) {
+        bindingsStream.on('data', function (data) {
+            movie_urls.push(data.get('v').value) //Variable ?v in the SPARQL query.
+        });
+        bindingsStream.on('end', function () {
+            engine.queryBindings(`SELECT ?name ?image WHERE {?movie <https://schema.org/name> ?name. ?movie <https://schema.org/image> ?image.}`, {
+                sources: movie_urls,
+            }).then(function (bindingsStream) {
+                bindingsStream.on('data', function (data) {
+                    let obj = {
+                        "name": data.get('name').value, //Variable ?name in the SPARQL query.
+                        "image": data.get('image').value //Variable ?image in the SPARQL query.
+                    };
+                    io.emit('update', {'message': obj}) //Send information to the browser.
+                });
+            });
+        })
     });
-    engine.queryBindings(``, { //TODO: complete SPARQL query
-    sources: movie_urls,
-  }).then(function (bindingsStream) {
-    bindingsStream.on('data', function (data) {
-      obj = {
-        "name": data.get('name').value,
-        "image": data.get('image').value
-      };
-      io.emit('update', {'message': obj})
+}
+
+async function handle_query_by_movie_name(keyword_url, keyword) {
+    let movie_urls = []; // List of movies in the pod
+
+    // Query to get all movie URLs
+    await engine.queryBindings('SELECT ?v WHERE { ?container <http://www.w3.org/ns/ldp#contains> ?v . }', {
+        sources: [keyword_url],
+    }).then(function (bindingsStream) {
+        bindingsStream.on('data', function (data) {
+            movie_urls.push(data.get('v').value);
+        });
     });
-  });
-  });
-  }
-  
-  
+    await engine.queryBindings(`SELECT ?name ?image WHERE { 
+                ?movie <https://schema.org/name> ?name .
+                ?movie <https://schema.org/image> ?image .
+                FILTER CONTAINS(LCASE(STR(?name)), LCASE(STR("${keyword}")))
+            }`, {
+        sources: movie_urls,
+    }).then(function (bindingsStream) {
+        bindingsStream.on('data', function (data) {
+            console.log('Element is: ', data.get('name').value)
+            let obj = {
+                "name": data.get('name').value,
+                "image": data.get('image').value
+            };
+            io.emit('update', {'message': obj});
+        });
+    });
+}
   
   httpServer.listen(port), 
       () => console.log("Server is running... on "+port);
